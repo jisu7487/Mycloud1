60ecf056860ecb789c63d11e434462b1ba834ef4
jisu7487 <jisu7487@naet.com>
2021-05-26 10:46:20 +0900

    start hi

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <section class="hero is-primary">
+      <div class="hero-body">
+        <p class="title">
+          강아지와 고양이의 동행
+        </p>
+        <p class="subtitle">
+          강아지와 고양이에 대한 모든 정보를 볼 수 있는 곳
+        </p>
+      </div>
+    </section>
+  </div>
+</template>
